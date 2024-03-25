1231fb5134dfca588b89cdda2642800af54adfae
PolarisSoda <urin187@gmail.com>
2024-03-25 21:11:05 +0900

    Update mytest.c

--- mytest.c
+++ mytest.c
@@ -6,6 +6,11 @@ int main() {
     for(int i=1; i<=20; i++) {
         if(setnice(i,12) == -1) printf(1,"Failed at %d\n",i);
     }
+    for(int i=1; i<=20; i++) {
+        int ret = getnice(i);
+        if(ret == -1) printf(1,"FAILED at %d\n",i);
+        else printf(1,"SUCCESS ON %d\n",ret);
+    }
     ps(0);
     ps(1);
     exit();
