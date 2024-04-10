5dd7d72d8e1659831bacb4f54b85260e20d32e2d
PolarisSoda <urin187@gmail.com>
2024-04-10 11:51:19 +0900

    werwer

--- mytest.c
+++ mytest.c
@@ -4,13 +4,5 @@
 
 int main() {
     ps(0);
-    for(int i=1; i<=10; i++) {
-        if(setnice(i,i) == -1) printf(1,"Failed!\n");
-    }
-    for(int i=1; i<=10; i++) {
-        int ret = getnice(i);
-        if(ret != -1) printf(1,"%d's nice value is %d\n",i,ret);
-    }
-    ps(0);
     exit();
 }
