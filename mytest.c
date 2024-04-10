c8d0e0445660c41dc476bdc25725a56d80132e60
PolarisSoda <urin187@gmail.com>
2024-04-10 13:09:32 +0900

    why it is printf

--- mytest.c
+++ mytest.c
@@ -8,7 +8,7 @@ int main() {
     int pid = fork();
     if(pid != 0) setnice(pid,0); 
     for(int i=1; i<=100000000; i++) for(int j=1; j<=100000000; j++) a++;
-    cprintf("%d : \n",a);
+    printf(1,"%d : \n",a);
     ps(0);
     exit();
 }
