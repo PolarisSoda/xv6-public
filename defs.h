9975a8393aa5c84f94ccf08abb8a5329d4b42226
PolarisSoda <urin187@gmail.com>
2024-05-12 00:30:05 +0900

    Updated PROJ3

--- defs.h
+++ defs.h
@@ -192,8 +192,8 @@ void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
-static pte_t*          walkpgdir(pde_t *pgdir, const void *va, int alloc);
-static int             mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
+pte_t*          walkpgdir(pde_t*,const void*,int);
+int             mappages(pde_t*, void*, uint, uint, int);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
