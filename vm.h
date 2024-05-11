95eb18ce48112b72ca6a871aa8127d925b763692
PolarisSoda <urin187@gmail.com>
2024-05-12 00:11:29 +0900

    Updated PROJ3

--- vm.h
+++ vm.h
@@ -4,7 +4,7 @@
 //#include "types.h"
 //#include "mmu.h"
 
-static int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
+int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
 pte_t* walkpgdir(pde_t *pgdir, const void *va, int alloc);
 
 #endif //XV6_PAGING_VM_H
