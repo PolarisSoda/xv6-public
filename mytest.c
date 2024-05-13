c49a4366086f42a3cac120463aa0161190b4bb1c
PolarisSoda <urin187@gmail.com>
2024-05-13 22:17:55 +0900

    Updated kalloc.c

--- mytest.c
+++ mytest.c
@@ -17,14 +17,21 @@ int main() {
     int fd = open("README",O_RDWR);
     if(fd == -1) exit();
     char* temp = (char*)mmap(0,4096,PROT_READ|PROT_WRITE,MAP_POPULATE,fd,0);
-    char s0 = temp[0];
-    char s10 = temp[10];
-    temp[0] = '1';
-    temp[10] = 0;
+    char s0 = temp[0],s10 = temp[10];
+    temp[0] = '1',temp[10] = 0;
     printf(1,"%s\n",temp);
-    temp[0] = s0;
-    temp[10] = s10;
+    temp[0] = s0,temp[10] = s10;
     printf(1,"Free Page : %d!\n",freemem());
-    exit();
-    
+
+    char* temp2 = (char*)mmap(4096,4096,PROT_READ|PROT_WRITE,MAP_ANONYMOUS,-1,0);
+    if(temp2 == 0) printf(1,"FAILED SOMETHING....\n"),exit();
+    printf(1,"Free Page : %d!\n",freemem());
+
+    char* temp3 = (char*)mmap(8192,4096,PROT_READ|PROT_WRITE,0,fd,1); //this will occur pagefault.
+    if(temp3 == 0) exit();
+    printf(1,"Free Page : %d!\n",freemem());
+
+    char* temp4 = (char*)mmap(8192,4096,PROT_READ|PROT_WRITE,MAP_POPULATE|MAP_ANONYMOUS,-1,0); //this will occur pagefault.
+    if(temp4 == 0) exit();
+    printf(1,"Free Page : %d!\n",freemem());
 }
