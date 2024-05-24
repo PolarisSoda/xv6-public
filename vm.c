c67f99b6613e386cb83d967c95b61f91987e54f9
PolarisSoda <urin187@gmail.com>
2024-05-24 17:25:53 +0900

    PROTO

--- vm.c
+++ vm.c
@@ -95,6 +95,7 @@ static int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm) {
       pages[idx].vaddr = a;
     } else {
       uint idx = pa/PGSIZE;
+      if(idx == 0) idx = 0;
       //if(idx < 0 || idx > PHYSTOP/PGSIZE) exit();
     }
     
