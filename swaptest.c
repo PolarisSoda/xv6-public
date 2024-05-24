8964adbfb2c17a71e0d26fd1e33d7bc4dbe19587
PolarisSoda <urin187@gmail.com>
2024-05-25 08:03:06 +0900

    PROJ4

--- swaptest.c
+++ swaptest.c
@@ -8,8 +8,12 @@
 #include "traps.h"
 #include "memlayout.h"
 
-
+extern struct page pages[PHYSTOP/4096];
+extern struct page *page_lru_head;
 int main () {
+    for(int i=0; i<PHYSTOP/4096; i++) {
+        printf(1,"%d %d\n",i,(int)pages[i].vaddr);
+    }
 	int a, b;
 
     swapstat(&a, &b);
