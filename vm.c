c72e3b42872438b6a6ec6c62ea37f90ed422c87f
PolarisSoda <urin187@gmail.com>
2024-05-25 11:03:52 +0900

    PROJ4

--- vm.c
+++ vm.c
@@ -360,11 +360,11 @@ clearpteu(pde_t *pgdir, char *uva)
 {
   pte_t *pte;
   pte = walkpgdir(pgdir, uva, 0);
-  cprintf("%x ",(int)pte);
+  cprintf("%x ",*pte);
   if(pte == 0)
     panic("clearpteu");
   *pte &= ~PTE_U;
-  cprintf("%x\n",(int)pte);
+  cprintf("%x\n",*pte);
 }
 
 // Given a parent process's page table, create a copy
