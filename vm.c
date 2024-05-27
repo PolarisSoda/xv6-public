c9129cc086bf5b77d50586970e2c883b640cede1
PolarisSoda <urin187@gmail.com>
2024-05-27 09:47:36 +0900

    Experimental

--- vm.c
+++ vm.c
@@ -98,11 +98,12 @@ int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm) {
         }
         num_lru_pages++;
         if(use_pages_lock) release(&pages_lock); //critical section ends.
-      }
-      struct page *cur = page_lru_head;
-      for(int i=0; i<num_lru_pages; i++) {
-        cprintf("%x %x -> ",cur->pgdir,cur->vaddr);
-        cur = cur->next;
+        
+        struct page *cur = page_lru_head;
+        for(int i=0; i<num_lru_pages; i++) {
+          cprintf("%x %x -> ",cur->pgdir,cur->vaddr);
+          cur = cur->next;
+        }
       }
       cprintf("\n");
     }
