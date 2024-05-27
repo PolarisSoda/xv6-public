d832fa845419a9ddd00fe7f72faee49252a7ad17
PolarisSoda <urin187@gmail.com>
2024-05-27 09:43:16 +0900

    Experimental

--- kalloc.c
+++ kalloc.c
@@ -116,6 +116,7 @@ int reclaim() {
           swapwrite(phy_addr,j<<3); //swap에 쓴다.
           swap_bit[j] = 0xFF; //썼다고 표시한다
           *now_pte = (PTE_FLAGS(*now_pte) & (~PTE_P)) | ((j+1)<<PTXSHIFT); //기존의 PTE에서 PPN대신 OFFSET으로 채워넣고, PTE_P 비트를 제거한다.
+          cprintf("%x\n",*now_pte);
           nl_kfree(phy_addr); //메모리에서 내용을 지운다.
           if(num_lru_pages == 1) {
             page_lru_head = 0;
