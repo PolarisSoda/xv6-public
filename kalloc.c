ecf429b2633d4564d99cbac6317429268115784f
PolarisSoda <urin187@gmail.com>
2024-05-27 12:32:38 +0900

    Experimental

--- kalloc.c
+++ kalloc.c
@@ -104,6 +104,7 @@ int reclaim() {
 
   for(int i=0; i<num_lru_pages; i++) {
     pte_t* now_pte = walkpgdir(page_lru_head->pgdir,page_lru_head->vaddr,0);
+    cprintf("%x %x %x\n",page_lru_head->pgdir,page_lru_head->vaddr,*now_pte);
     if(!now_pte) continue;
     cprintf("%x\n",*now_pte);
     if(*now_pte&PTE_P) continue;
