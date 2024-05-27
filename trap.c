25031c43dd75331c71e9e141d97b2b25aa7f4690
PolarisSoda <urin187@gmail.com>
2024-05-27 22:09:36 +0900

    Experimental

--- trap.c
+++ trap.c
@@ -121,7 +121,6 @@ trap(struct trapframe *tf)
 
   //PAGEBREAK: 13
   default:
-    defe:
     if(myproc() == 0 || (tf->cs&3) == 0){
       // In kernel, it must be our mistake.
       cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
